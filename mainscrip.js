e53a787e975ee613ceeb11f8c24c91ff444865fe
Lucas Marais <lucas.marais.wrk@gmail.com>
2023-03-08 16:47:40 +0100

    new features and corrected navbar

--- mainscrip.js
+++ mainscrip.js
@@ -1,34 +1,23 @@
 window.onscroll = function() {myFunction()};
 
+
+
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+    navbar.style.overflow = "visible";
   } else {
     navbar.classList.remove("sticky");
+    navbar.style.overflow = "hidden";
+
   }
 }
 
 
+function addTile(){
+  document.getElementById('box').innerHTML += '<div id="box"><div class="tile"><div class="tile-inner"><div class="tile-front"><h3>Card 1</h3><p>Some text</p><p>Some text</p></div><div class="tile-back"><h3>Card 1 back</h3><p>Some new text</p><p>Some new text</p></div></div></div></div>';
+}
 
-
-
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
-
-// // Get the navbar
-// var navbar = document.getElementById("navbar");
-
-// // Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
-
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// } 
